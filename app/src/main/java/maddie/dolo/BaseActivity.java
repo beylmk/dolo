f6637f4fc241cef558d544292010415f3540cc5b
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2018-08-16 14:00:51 -0700

    add google-services to gitignore, add logout button to nav drawer, initial widget setup

--- app/src/main/java/maddie/dolo/BaseActivity.java
+++ app/src/main/java/maddie/dolo/BaseActivity.java
@@ -2,6 +2,7 @@ package maddie.dolo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,6 +12,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -62,6 +68,17 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigati
         } else if (id == R.id.nav_get_there && !(this instanceof GetThereActivity)) {
             Intent getThereIntent = new Intent(this, GetThereActivity.class);
             startActivity(getThereIntent);
+        } else if (id == R.id.nav_logout) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener() {
+                        public void onComplete(@NonNull Task task) {
+                            Intent logoutIntent = new Intent(BaseActivity.this, LoginActivity.class);
+                            logoutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(logoutIntent);
+                            Toast.makeText(BaseActivity.this, "Logged out", Toast.LENGTH_LONG).show();
+                        }
+                    });
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
